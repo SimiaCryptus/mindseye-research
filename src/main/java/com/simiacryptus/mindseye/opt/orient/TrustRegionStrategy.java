9740c65f0cd77da957f2ab81b56ed98101fc3dc0
Andrew Charneski <acharneski@gmail.com>
2019-04-04 21:08:17 -0700

    wip

--- src/main/java/com/simiacryptus/mindseye/opt/orient/TrustRegionStrategy.java
+++ src/main/java/com/simiacryptus/mindseye/opt/orient/TrustRegionStrategy.java
@@ -19,6 +19,7 @@
 
 package com.simiacryptus.mindseye.opt.orient;
 
+import com.simiacryptus.lang.ref.ReferenceCountingBase;
 import com.simiacryptus.mindseye.eval.Trainable;
 import com.simiacryptus.mindseye.lang.DeltaSet;
 import com.simiacryptus.mindseye.lang.DoubleBuffer;
@@ -87,6 +88,7 @@ public abstract class TrustRegionStrategy extends OrientationStrategyBase<LineSe
 
   @Override
   protected void _free() {
+    history.forEach(ReferenceCountingBase::freeRef);
     this.inner.freeRef();
   }
 
@@ -122,116 +124,132 @@ public abstract class TrustRegionStrategy extends OrientationStrategyBase<LineSe
   @Nonnull
   @Override
   public LineSearchCursor orient(@Nonnull final Trainable subject, final PointSample origin, final TrainingMonitor monitor) {
-    history.add(0, origin);
+    history.add(0, origin.addRef());
     while (history.size() > maxHistory) {
-      history.remove(history.size() - 1);
+      history.remove(history.size() - 1).freeRef();
     }
     final SimpleLineSearchCursor cursor = inner.orient(subject, origin, monitor);
-    return new LineSearchCursorBase() {
-      @Nonnull
-      @Override
-      public CharSequence getDirectionType() {
-        return cursor.getDirectionType() + "+Trust";
-      }
+    return new TrustRegionCursor(cursor, subject);
+  }
 
-      @Nonnull
-      @Override
-      public DeltaSet<UUID> position(final double alpha) {
-        reset();
-        @Nonnull final DeltaSet<UUID> adjustedPosVector = cursor.position(alpha);
-        project(adjustedPosVector, new TrainingMonitor());
-        return adjustedPosVector;
-      }
+  @Override
+  public void reset() {
+    inner.reset();
+  }
 
-      public Layer toLayer(UUID id) {
-        DAGNetwork layer = (DAGNetwork) subject.getLayer();
-        if(null == layer) return null;
-        return layer.getLayersById().get(id);
-      }
+  private class TrustRegionCursor extends LineSearchCursorBase {
+    private final SimpleLineSearchCursor cursor;
+    private final Trainable subject;
 
-      @Nonnull
-      public DeltaSet<UUID> project(@Nonnull final DeltaSet<UUID> deltaIn, final TrainingMonitor monitor) {
-        final DeltaSet<UUID> originalAlphaDerivative = cursor.direction;
-        @Nonnull final DeltaSet<UUID> newAlphaDerivative = originalAlphaDerivative.copy();
-        deltaIn.getMap().forEach((id, buffer) -> {
-          @Nullable final double[] delta = buffer.getDelta();
-          if (null == delta) return;
-          final double[] currentPosition = buffer.target;
-          @Nullable final double[] originalAlphaD = originalAlphaDerivative.get(id, currentPosition).getDeltaAndFree();
-          @Nullable final double[] newAlphaD = newAlphaDerivative.get(id, currentPosition).getDeltaAndFree();
-          @Nonnull final double[] proposedPosition = ArrayUtil.add(currentPosition, delta);
-          final TrustRegion region = getRegionPolicy(toLayer(id));
-          if (null != region) {
-            final Stream<double[]> zz = history.stream().map((@Nonnull final PointSample x) -> {
-              final DoubleBuffer<UUID> d = x.weights.getMap().get(id);
-              @Nullable final double[] z = null == d ? null : d.getDelta();
-              return z;
-            });
-            final double[] projectedPosition = region.project(zz.filter(x -> null != x).toArray(i -> new double[i][]), proposedPosition);
-            if (projectedPosition != proposedPosition) {
-              for (int i = 0; i < projectedPosition.length; i++) {
-                delta[i] = projectedPosition[i] - currentPosition[i];
-              }
-              @Nonnull final double[] normal = ArrayUtil.subtract(projectedPosition, proposedPosition);
-              final double normalMagSq = ArrayUtil.dot(normal, normal);
+    public TrustRegionCursor(SimpleLineSearchCursor cursor, Trainable subject) {
+      this.cursor = cursor;
+      this.subject = subject;
+    }
+
+    @Override
+    public PointSample afterStep(@Nonnull PointSample step) {
+      super.afterStep(step);
+      return cursor.afterStep(step);
+    }
+
+    @Nonnull
+    @Override
+    public CharSequence getDirectionType() {
+      return cursor.getDirectionType() + "+Trust";
+    }
+
+    @Nonnull
+    @Override
+    public DeltaSet<UUID> position(final double alpha) {
+      //reset();
+      @Nonnull final DeltaSet<UUID> adjustedPosVector = cursor.position(alpha);
+      project(adjustedPosVector, new TrainingMonitor());
+      return adjustedPosVector;
+    }
+
+    public Layer toLayer(UUID id) {
+      DAGNetwork layer = (DAGNetwork) subject.getLayer();
+      if (null == layer) return null;
+      return layer.getLayersById().get(id);
+    }
+
+    @Nonnull
+    public DeltaSet<UUID> project(@Nonnull final DeltaSet<UUID> deltaIn, final TrainingMonitor monitor) {
+      final DeltaSet<UUID> originalAlphaDerivative = cursor.direction;
+      @Nonnull final DeltaSet<UUID> newAlphaDerivative = originalAlphaDerivative.copy();
+      deltaIn.getMap().forEach((id, buffer) -> {
+        @Nullable final double[] delta = buffer.getDelta();
+        if (null == delta) return;
+        final double[] currentPosition = buffer.target;
+        @Nullable final double[] originalAlphaD = originalAlphaDerivative.get(id, currentPosition).getDeltaAndFree();
+        @Nullable final double[] newAlphaD = newAlphaDerivative.get(id, currentPosition).getDeltaAndFree();
+        @Nonnull final double[] proposedPosition = ArrayUtil.add(currentPosition, delta);
+        final TrustRegion region = getRegionPolicy(toLayer(id));
+        if (null != region) {
+          final Stream<double[]> zz = history.stream().map((@Nonnull final PointSample pointSample) -> {
+            final DoubleBuffer<UUID> d = pointSample.weights.getMap().get(id);
+            @Nullable final double[] z = null == d ? null : d.getDelta();
+            return z;
+          });
+          final double[] projectedPosition = region.project(zz.filter(x -> null != x).toArray(i -> new double[i][]), proposedPosition);
+          if (projectedPosition != proposedPosition) {
+            for (int i = 0; i < projectedPosition.length; i++) {
+              delta[i] = projectedPosition[i] - currentPosition[i];
+            }
+            @Nonnull final double[] normal = ArrayUtil.subtract(projectedPosition, proposedPosition);
+            final double normalMagSq = ArrayUtil.dot(normal, normal);
 //              monitor.log(String.format("%s: evalInputDelta = %s, projectedPosition = %s, proposedPosition = %s, currentPosition = %s, normalMagSq = %s", key,
 //                ArrayUtil.dot(evalInputDelta,evalInputDelta),
 //                ArrayUtil.dot(projectedPosition,projectedPosition),
 //                ArrayUtil.dot(proposedPosition,proposedPosition),
 //                ArrayUtil.dot(currentPosition,currentPosition),
 //                normalMagSq));
-              if (0 < normalMagSq) {
-                final double a = ArrayUtil.dot(originalAlphaD, normal);
-                if (a != -1) {
-                  @Nonnull final double[] tangent = ArrayUtil.add(originalAlphaD, ArrayUtil.multiply(normal, -a / normalMagSq));
-                  for (int i = 0; i < tangent.length; i++) {
-                    newAlphaD[i] = tangent[i];
-                  }
+            if (0 < normalMagSq) {
+              final double a = ArrayUtil.dot(originalAlphaD, normal);
+              if (a != -1) {
+                @Nonnull final double[] tangent = ArrayUtil.add(originalAlphaD, ArrayUtil.multiply(normal, -a / normalMagSq));
+                for (int i = 0; i < tangent.length; i++) {
+                  newAlphaD[i] = tangent[i];
+                }
 //                  double newAlphaDerivSq = ArrayUtil.dot(tangent, tangent);
 //                  double originalAlphaDerivSq = ArrayUtil.dot(originalAlphaD, originalAlphaD);
 //                  assert(newAlphaDerivSq <= originalAlphaDerivSq);
 //                  assert(Math.abs(ArrayUtil.dot(tangent, normal)) <= 1e-4);
 //                  monitor.log(String.format("%s: normalMagSq = %s, newAlphaDerivSq = %s, originalAlphaDerivSq = %s", key, normalMagSq, newAlphaDerivSq, originalAlphaDerivSq));
-                }
               }
-
-
             }
+
+
           }
-        });
-        return newAlphaDerivative;
-      }
+        }
+      });
+      return newAlphaDerivative;
+    }
 
-      @Override
-      public void reset() {
-        cursor.reset();
-      }
+    @Override
+    public void reset() {
+      cursor.reset();
+    }
 
-      @Nonnull
-      @Override
-      public LineSearchPoint step(final double alpha, final TrainingMonitor monitor) {
-        cursor.reset();
-        @Nonnull final DeltaSet<UUID> adjustedPosVector = cursor.position(alpha);
-        @Nonnull final DeltaSet<UUID> adjustedGradient = project(adjustedPosVector, monitor);
-        adjustedPosVector.accumulate(1);
-        adjustedPosVector.freeRef();
-        @Nonnull final PointSample sample = subject.measure(monitor).setRate(alpha);
-        double dot = adjustedGradient.dot(sample.delta);
-        adjustedGradient.freeRef();
-        LineSearchPoint lineSearchPoint = new LineSearchPoint(sample, dot);
-        sample.freeRef();
-        return lineSearchPoint;
-      }
+    @Nonnull
+    @Override
+    public LineSearchPoint step(final double alpha, final TrainingMonitor monitor) {
+      cursor.reset();
+      @Nonnull final DeltaSet<UUID> adjustedPosVector = cursor.position(alpha);
+      @Nonnull final DeltaSet<UUID> adjustedGradient = project(adjustedPosVector, monitor);
+      adjustedPosVector.accumulate(1);
+      adjustedPosVector.freeRef();
+      @Nonnull final PointSample sample = afterStep(subject.measure(monitor).setRate(alpha));
+      double dot = adjustedGradient.dot(sample.delta);
+      adjustedGradient.freeRef();
+      LineSearchPoint lineSearchPoint = new LineSearchPoint(sample, dot);
+      sample.freeRef();
+      return lineSearchPoint;
+    }
 
-      @Override
-      public void _free() {
-        cursor.freeRef();
-      }
-    };
-  }
-
-  @Override
-  public void reset() {
-    inner.reset();
+    @Override
+    public void _free() {
+      cursor.freeRef();
+    }
   }
 }
